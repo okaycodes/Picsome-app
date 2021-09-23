f88047c22ca7f534e3c08152568d752339253bdc
okaycodes <fo.okenana@gmail.com>
2021-09-23 22:11:18 +0100

    create MainContent.js

---
+++ src/components/MainContent.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react"
+
+class MainContent extends Component{
+  constructor(){
+    super()
+    this.state = {}
+  }
+
+  render (){
+    return(
+      <div>Place Holder Main content</div>
+    )
+  }
+}
+
+
+export default MainContent
