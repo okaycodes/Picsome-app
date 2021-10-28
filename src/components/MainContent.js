30d1af696f60f16607beacbbeee55ed66b3ff032
okaycodes <fo.okenana@gmail.com>
2021-10-28 17:42:58 +0100

    fetch and display pictures

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -6,10 +6,24 @@ import heartFilled from "./../icons/heart-filled.svg"
 
 
 function MainContent(){
+  const [photosData, setPhotosData] = useState([])
 
+  useEffect(()=>{
+    fetch("https://picsum.photos/v2/list?page=2&limit=10")
+      .then(response=>response.json())
+      .then(data=>setPhotosData(data))
+  }, [])
+  const photos = photosData.map(i=>
+    <div key={i.id} className="photo" style={{backgroundImage: `url(${i.download_url})`}}>
+      <div className="photo__actions">
+        <img className="favorite" src={heartThin} alt=""/>
+        <img className="add" src={add} alt=""/>
+      </div>
+    </div>
+  )
   return(
     <div className="photosCollage">
-
+      {photos}
     </div>
   )
 }
