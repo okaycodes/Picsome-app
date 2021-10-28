39c44d00ccff4ba0d5800a144be27d21b6397c96
okaycodes <fo.okenana@gmail.com>
2021-10-28 09:20:18 +0100

    update App: add Switch and Route to MainContent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import Header from "./components/Header"
 import MainContent from "./components/MainContent"
+import {Switch, Route} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <MainContent />
+
+      <Switch>
+        <Route path="/">
+          <MainContent />
+        </Route>
+      </Switch>
     </div>
   );
 }
