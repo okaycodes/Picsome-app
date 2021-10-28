2de6deaea6ed02454308c52d4ab6c5b16ddddb53
okaycodes <fo.okenana@gmail.com>
2021-10-28 09:18:27 +0100

    update Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,16 @@
 import React from "react"
+import {Link} from "react-router-dom"
+
+import icon from "./../icons/shopping-cart.svg"
 
 function Header(){
   return (
-    <div>Place holder header</div>
+    <div className="header">
+      <div className="header__container">
+        <h1><Link className="header__link" to="/">Pic Some</Link></h1>
+        <Link><img className="icon" src={icon} alt=""/></Link>
+      </div>
+    </div>
   )
 }
 
