36d615f881ee9ced7d591c1bc5a34c4873d52518
okaycodes <fo.okenana@gmail.com>
2021-10-29 10:12:02 +0100

    update App: handleClick, new var isFavorited to JSON Data

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -8,19 +8,45 @@ import heartFilled from "./../icons/heart-filled.svg"
 function MainContent(){
   const [photosData, setPhotosData] = useState([])
 
+  const handleClick=(id)=>{
+    const editedData =  photosData.map(i=>{
+      if(i.id===id){
+          return {...i, isFavorited: !i.isFavorited}
+      }
+      return i
+    })
+    setPhotosData(editedData)
+  }
+
+
   useEffect(()=>{
     fetch("https://picsum.photos/v2/list?page=2&limit=10")
       .then(response=>response.json())
-      .then(data=>setPhotosData(data))
+      .then(data=>{
+        // add a new variable isFavorited before setting state
+        const withFavoriteVar = data.map(i=>({...i, isFavorited: false}))
+        setPhotosData(withFavoriteVar)
+      })
   }, [])
-  const photos = photosData.map(i=>
-    <div key={i.id} className="photo" style={{backgroundImage: `url(${i.download_url})`}}>
-      <div className="photo__actions">
-        <img className="favorite" src={heartThin} alt=""/>
-        <img className="add" src={add} alt=""/>
+
+    const photos = photosData.map(i=>
+      <div key={i.id} className="photo" style={{backgroundImage: `url(${i.download_url})`}}>
+        <div className="photo__actions">
+          <img
+          className={i.isFavorited ?"isLiked":"like"}
+          src={i.isFavorited ? heartFilled:heartThin}
+          alt=""
+          onClick={()=>handleClick(i.id)}
+          />
+          <img
+          className="add"
+          src={add}
+          alt=""
+          />
+        </div>
       </div>
-    </div>
-  )
+    )
+
   return(
     <div className="photosCollage">
       {photos}
