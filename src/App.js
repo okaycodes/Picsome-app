b6f358494c7e65b6dc1461f8f811b894b8b6f20c
okaycodes <fo.okenana@gmail.com>
2021-10-29 09:48:31 +0100

    update App: Route to details and Cart pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
+import {useState} from "react"
 import './App.css';
 import Header from "./components/Header"
+import Footer from "./components/Footer"
 import MainContent from "./components/MainContent"
+import Details from "./pages/Details"
+import Cart from "./pages/Cart"
 import {Switch, Route} from "react-router-dom"
 
 function App() {
+
   return (
     <div className="App">
-      <Header />
+      <div className="wrapper">
+        <Header />
 
-      <Switch>
-        <Route path="/">
-          <MainContent />
-        </Route>
-      </Switch>
+        <Switch>
+          <Route exact path="/">
+            <MainContent />
+          </Route>
+
+          <Route path="/details/:productId">
+            <Details />
+          </Route>
+
+          <Route path="/cart">
+            <Cart />
+          </Route>
+        </Switch>
+      </div>
+
+      <Footer />
     </div>
   );
 }
